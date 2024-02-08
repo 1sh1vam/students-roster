b9dbc940cf41d4f89dca6e2c2e380dd2fe91d278
1sh1vam <shivam07.mt@gmail.com>
2024-02-08 12:24:00 +0530

    fix: add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 import { studentsRouter } from './routes/students';
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.use(studentsRouter);
